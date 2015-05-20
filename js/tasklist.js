1bf7cfb38dfa1deafe9fa15dea4f9c4558e4386c
Kristine Domingo <kristine@cs.utexas.edu>
2015-05-20 16:30:05 -0500

    Redoing, reverting back to non-app, non-websocket form.

--- js/tasklist.js
+++ js/tasklist.js
@@ -1,78 +1,45 @@
-var high = [];
-var mid = [];
-var low = [];
-
 var count = 0;
 
-function Task (task, priority, id)
+$(document).ready(function()
 {
-	this.task = task;
-	this.priority = priority;
-	this.ID = "task" + count;
-	this.HTML = $("<p id='" + this.ID + "'> [x] " + this.task + "</p>");
-}
+  var taskList;
+  var task;
+  var highPriorityDiv = $("#highPriority");
+  var midPriorityDiv = $("#midPriority");
+  var lowPriorityDiv = $("#lowPriority");
 
-$(document).ready (function ()
-{
-	var highPriorityDiv = $("#highPriority");
-	var midPriorityDiv = $("#midPriority");
-	var lowPriorityDiv = $("#lowPriority");
+  $("#add").on("click", function()
+  {
+    var taskToAdd = $("#taskInput").val();      
+    var priority = $("input[name=priority]:checked").val();
 
-	$("#addForm").submit (function ()
-	{
-		var taskToAdd = $("#taskInput").val ();		
-		var priority = $("input[name=priority]:checked").val ();
+    if(priority === "high")
+    {
+      taskList = highPriorityDiv;
+    }
+    else if(priority === "mid")
+    {
+      taskList = midPriorityDiv
+    }
+    else if(priority === "low")
+    {
+      taskList = lowPriorityDiv;
+    }   
+    else
+    {
+      alert("Select a priority!");
+      return false;
+    }
 
-		if (priority === "high")
-		{
-			high[high.length] = new Task (taskToAdd, priority, count);
-			highPriorityDiv.html ("");
-			high.forEach (function (task)
-			{
-				highPriorityDiv.append (task.HTML);
+    taskList.append('<div id="task' + count + '">[X] ' + taskToAdd + '</div>');
+    task = $("#task" + count);
 
-				$("#" + task.ID).on ("click", function ()
-				{
-					$(this).html ("");
-				});
+    task.on("click", function()
+    {
+      $(this).remove();
+    });
 
-			});
-
-		}
-		else if (priority === "mid")
-		{
-			mid[mid.length] = new Task (taskToAdd, priority, count);
-			midPriorityDiv.html ("");
-			mid.forEach (function (task)
-			{
-				midPriorityDiv.append (task.HTML);
-
-				$("#" + task.ID).on ("click", function ()
-				{
-					$(this).html ("");
-				});
-			});
-		}
-		else if (priority === "low")
-		{
-			low[low.length] = new Task (taskToAdd, priority, count);
-			lowPriorityDiv.html ("");
-			low.forEach (function (task)
-			{
-				lowPriorityDiv.append (task.HTML);
-
-				$("#" + task.ID).on ("click", function ()
-				{
-					$(this).html ("");
-				});
-			});
-		}	
-		else
-		{
-			alert ("Select a priority!");
-		}
-
-		count++;
-		return false;
-	});
+    count++;
+    return false;
+  });
 });
