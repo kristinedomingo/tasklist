6708250abc07dcd7a770004058edb1a6b27b3bf7
Kristine Domingo <kristine@cs.utexas.edu>
2015-05-21 12:55:42 -0500

    Style changes and jquery include fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express = require ("express");
+var express = require ('express');
 var app = express ();
 
-app.set ("views", __dirname + "/views");
-app.set ("view engine", "ejs");
-app.engine ("html", require ("ejs").renderFile);
-app.use (express.static (__dirname + "/public"));
+app.set ('views', __dirname + '/views');
+app.set ('view engine', 'ejs');
+app.engine ('html', require ('ejs').renderFile);
+app.use (express.static (__dirname + '/public'));
 
-app.get ("/", function (req, res)
+app.get ('/', function (req, res)
 {
-  res.render ("index.html");
+  res.render ('index.html');
 });
 
 app.listen (3000);
